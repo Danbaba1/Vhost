ad6b01ee94993bf994329386c6060752fb596e7d
Daniel Oladepo <oladepodaniel69@gmail.com>
2024-09-07 17:01:31 +0100

    Create Vhost server

---
+++ user-subdomains.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import vhost from 'vhost'
+const app = express()
+
+const users = express.Router()
+users.get('/', (req, res, next) => {
+    const username = req
+    .vhost[0]
+    .split('-')
+    .map(name => (
+        name[0].toUpperCase() +
+        name.slice(1)
+    ))
+    .join(' ')
+    res.send(`Hello, ${username}`);
+});
+
+app.use(vhost('*.localhost', users))
+
+app.listen(
+    1337,
+    () => console.log('Web Server running on port 1337'),
+)
