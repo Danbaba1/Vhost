799eefc8e7c9d6431d7150969eed779827ba090e
Daniel Oladepo <oladepodaniel69@gmail.com>
2024-09-17 16:04:50 +0100

    Add new endpoints

--- user-subdomains.js
+++ user-subdomains.js
@@ -5,15 +5,30 @@ const app = express()
 const users = express.Router()
 users.get('/', (req, res, next) => {
     const username = req
-    .vhost[0]
-    .split('-')
-    .map(name => (
-        name[0].toUpperCase() +
-        name.slice(1)
-    ))
-    .join(' ')
+        .vhost[0]
+        .split('-')
+        .map(name => (
+            name[0].toUpperCase() +
+            name.slice(1)
+        ))
+        .join(' ')
     res.send(`Hello, ${username}`);
 });
+
+// Additional dynamic routes
+users.get('/profile', (req, res, next) => {
+    const username = req.vhost[0].split('-')[0];
+    res.send(`Profile page for ${username}`);
+});
+
+users.get('/update', (req, res, next) => {
+    // Extract the username from the virtual host
+    const username = req.vhost[0].split('-')[0];
+    
+    // Send a response with the username
+    res.send(`Update received for ${username}`);
+});
+
 
 app.use(vhost('*.localhost', users))
 
--- virtual-domains.js
+++ virtual-domains.js
@@ -5,13 +5,27 @@ const app = express()
 const app1 = express.Router()
 const app2 = express.Router()
 
+// Routes for app1
 app1.get('/', (req, res, next) => {
     res.send('This is the main application.');
-});
-
-app2.get('/', (req, res, next) => {
+  });
+  app1.get('/about', (req, res, next) => {
+    res.send('About page for the main application.');
+  });
+  app1.get('/contact', (req, res, next) => {
+    res.send('Contact page for the main application.');
+  });
+  
+  // Routes for app2
+  app2.get('/', (req, res, next) => {
     res.send('This is a second application.');
-});
+  });
+  app2.get('/info', (req, res, next) => {
+    res.send('Info page for the second application.');
+  });
+  app2.get('/services', (req, res, next) => {
+    res.send('Services page for the second application.');
+  });
 
 app.use(vhost('localhost', app1));
 app.use(vhost('second.localhost', app2));
